99479896b84259d9fc18c9f61bea17e2c00ad8e9
tamara <tamarakur23@gmail.com>
2024-01-14 01:23:54 +0100

    new

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,6 @@
-const day_night = document.querySelectorAll('.version');
-const icon = document.querySelectorAll('.version svg use');
 const menu_open = document.querySelector('.ham');
 const menu_close = document.querySelector('.close');
 const phone_menu = document.getElementById('phone-menu');
-
-const night = document.querySelectorAll('.night');
-const day = document.querySelectorAll('.day');
 
 menu_open.addEventListener('click', () =>
 {
@@ -16,34 +11,3 @@ menu_close.addEventListener('click', () =>
 {
     phone_menu.style.display = 'none';
 })
-
-
-
-day_night.forEach(version => (
-    version.addEventListener('click', () =>
-{
-    if(document.querySelector('body').classList.contains('dark'))
-    {
-        document.querySelector('body').classList.remove('dark');
-        night.forEach(element => {
-            element.style.display = 'none';
-        });
-        day.forEach(element => {
-            element.style.display = 'block';
-        });
-        icon.forEach(element => {
-            element.setAttribute('xlink:href', './img/sprite.svg#dark');
-        });
-    } else {
-        document.querySelector('body').classList.add('dark');
-        night.forEach(element => {
-            element.style.display = 'block';
-        });
-        day.forEach(element => {
-            element.style.display = 'none';
-        });
-        icon.forEach(element => {
-            element.setAttribute('xlink:href', './img/sprite.svg#light');
-        });
-    }
-})));
---
+++ scripts/other_theme.js
@@ -0,0 +1,55 @@
+if (localStorage.getItem("theme"))
+    document.querySelector('body').classList.toggle(localStorage.getItem("theme")); //get theme from local storage and set it
+
+const day_night = document.querySelectorAll('.version');
+const icon = document.querySelectorAll('.version svg use');
+
+const night = document.querySelectorAll('.night');
+const day = document.querySelectorAll('.day');
+
+if(document.querySelector('body').classList.contains('dark')) {
+    icon.forEach(element => {
+        element.setAttribute('xlink:href', '../img/sprite.svg#light');
+    });
+} else {
+    icon.forEach(element => {
+        element.setAttribute('xlink:href', '../img/sprite.svg#dark');
+    });
+
+}
+
+
+day_night.forEach(version => (
+    version.addEventListener('click', () =>
+{
+    if(document.querySelector('body').classList.contains('dark'))
+    {
+        document.querySelector('body').classList.remove('dark');
+        night.forEach(element => {
+            element.style.display = 'none';
+        });
+        day.forEach(element => {
+            element.style.display = 'block';
+        });
+        icon.forEach(element => {
+            element.setAttribute('xlink:href', '../img/sprite.svg#dark');
+        });
+
+        let actualTheme = document.querySelector('body').className;
+        localStorage.setItem('theme', actualTheme);
+    } else {
+        document.querySelector('body').classList.add('dark');
+        night.forEach(element => {
+            element.style.display = 'block';
+        });
+        day.forEach(element => {
+            element.style.display = 'none';
+        });
+        icon.forEach(element => {
+            element.setAttribute('xlink:href', '../img/sprite.svg#light');
+        });
+
+        let actualTheme = document.querySelector('body').className;
+        localStorage.setItem('theme', actualTheme);
+    }
+})));
